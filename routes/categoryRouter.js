da4410de0c11fb4eb488564b0b91cff32ded437f
tai <nguyentientai10@gmail.com>
2022-01-28 16:14:19 +0700

    make it better

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const categoryCtrl = require("../controllers/categoryCtrl");
+const categoryCtrl = require("../Controllers/categoryCtrl");
 const auth = require("../middleware/auth");
 const authAdmin = require("../middleware/authAdmin");
 
